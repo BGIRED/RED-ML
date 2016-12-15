bffabbbcfcf0c31a7df529455dfc1a3f7870ecb3
Heng Xiong <xiongheng@genomics.cn>
2016-12-15 10:13:16 +0800

    Update red_ML.pl

--- bin/red_ML.pl
+++ bin/red_ML.pl
@@ -7,42 +7,46 @@ my $usage=<<USAGE;
 
      Description:
 
-         This program is used to identify RNA editing sites based on machine learning.  All of the programs were located in directory of bin when the software package was decompressed. The software package could only run on linux platform currently and the main program is red_ML.pl.
+        RED-ML is a software tool to do genome-wide RNA editing dectection (RED) based on RNA-seq data. All source codes and executables are located in the "bin" directory. The tool can be run on a Linux platform and the main program is red_ML.pl.
     
     Parameters:
         
-        --rnabam       [STR] the RNA alignment file which is used to calling RNA editing sites.
-        --reference    [STR] the genome fasta file(hg19.fa).
-        --dbsnp        [STR] SNP database, eg dbsnp138.
-        --simpleRepeat [STR] genome simple repeat region annotation file, should be bed format.
-        --alu          [STR] genome alu region annotation file, should be bed format.
-        --snplist      [STR] a file included konwn SNP sites, the first two column should be chromosome, position and seperated by Tab, it could be used to remove SNP [optional].
+        --rnabam       [STR] the sorted BAM file obtained from RNA-seq to detect RNA editing sites.
+        --reference    [STR] the fasta file containing the reference genome, e.g., hg19.fa.
+        --dbsnp        [STR] the SNP database file, e.g., dbSNP138.
+        --simpleRepeat [STR] genome-wide simple repeat annotation, should be in BED format.
+        --alu          [STR] genome-wide Alu repeat annotation, should be in BED format.
+        --snplist      [STR] a tab-delimited file listing known SNPs, with the first two columns being chromosome and position of each SNP [optional].
         --outdir       [STR] the directory of output.
-        --p            [NUM] a number between 0 and 1 [default 0.5]; 
-        --help         [STR] show this information!
+        --p            [NUM] the detection threshold, a number between 0 and 1 [default 0.5];
+        --help         [STR] show this help information!
     
     Example:
+    
         perl $0 --rnabam in.bam --reference hg19.fa --dbsnp dbsnp138.vcf --simpleRepeat hg19_simpleRepeat.reg.bed --alu hg19.alu.bed --snplist snp.list --outdir outdir
      
     Requirements:
 
         Before running this program, several pieces of database are needed.
+        The reference genome (hg19), downloaded from: http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes.
         dbSNP138, downloaded from:http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database.
-        simpleRepeat, downloaded from http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database.
+        simpleRepeat, downloaded from http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database, and then do:
         awk '{print \$2"\\t"\$3"\\t"\$4}' simpleRepeat.txt > simpleRepeat.bed
         bedtools merge -i simpleRepeat.bed > simpleRepeat.merge.bed
-        Alu, downloaded from http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database.
+        Alu, downloaded from http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database, and then do:
         grep Alu rmsk.txt | awk '{print \$6"\\t"\$7"\\t"\$8}' > hg19.alu.bed
+        We have also provided the simpleRepeat and Alu files under the "database" directory for the user's convenience.
 
     Outputs:
  
-        When the program running completed, three files would be produced in the output directory.
-        RNA_editing.sites.txt is the all reliable RNA editing sites file;
-        variation.sites.feature.txt is the all varitaion sites file which contain feature values;
-        mut.txt.gz is the all varitaion sites file which contain pileup information;
-
+        When the program finishes running, three files will be created in the output directory. 
+        RNA_editing.sites.txt lists all detected RNA editing sites that pass the detection threshold p;
+        variation.sites.feature.txt lists all variant sites with associated feature values; 
+        mut.txt.gz contains all variant sites with pileup information.
+    
     Notice:
-        The input bam should be indexed, you could use samtools to create index.
+    
+        The input bam should be sorted (indexed), you could use samtools to create index.
         samtools index in.bam
 
 USAGE
